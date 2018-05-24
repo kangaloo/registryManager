39814730a00d527d8fff1a616923e93213f9592b
Xiangyang Li <13622004447@163.com>
2018-05-24 16:41:56 +0800

    Import go-prompt

--- subcommands/prompt.go
+++ subcommands/prompt.go
@@ -5,10 +5,10 @@ import "github.com/c-bata/go-prompt"
 func completer(d prompt.Document) []prompt.Suggest {
 
 	s := []prompt.Suggest{
-		{Text: "show", Description: ""},
-		{Text: "set", Description: ""},
-		{Text: "exit", Description: ""},
-		{Text: "quit", Description: ""},
+		{Text: "show", Description: "List all docker insecure registries"},
+		{Text: "set", Description: "Reset the docker config path"},
+		{Text: "exit", Description: "Quit this manager"},
+		{Text: "quit", Description: "Quit this manager"},
 	}
 	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
 }
